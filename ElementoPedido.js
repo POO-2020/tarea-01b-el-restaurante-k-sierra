f9228b6e312b45d52bcc4cb030f476738802ecd7
Karla Sierra <ksierra@ucol.mx>
2020-02-25 02:46:04 -0600

    Elemento pedido final

---
+++ ElementoPedido.js
@@ -0,0 +1,17 @@
+import Precio from "./Precio.js";
+export default class elementoPedido{
+    /**
+     * 
+     * @param {string} producto 
+     * @param {number} cantidad 
+     * @param {number} precio
+     */
+    constructor(producto, cantidad, precio){
+        this.productos = producto;
+        this.cantidades = cantidad;
+        this.precios = precio;
+    }
+    getDescripccion(){
+        return `${this.cantidades} x ${this.productos.nombres} = ${this.productos.precios.valores*this.cantidades}`;
+    }
+}
